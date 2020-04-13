1cdf3b36ae2751f55727e252382964744963d1c7
yanchi <li.yanc@husky.neu.edu>
2020-04-13 19:13:33 -0400

    boilerplate from project 2

---
+++ src/main/java/Cs5200Spring2020YanchiApplication.java
@@ -0,0 +1,25 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.boot.web.support.SpringBootServletInitializer;
+//if you face an error with above imports, try
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+
+@SpringBootApplication
+public class Cs5200Spring2020YanchiApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder
+	configure(SpringApplicationBuilder application) {
+		return application.sources(
+				Cs5200Spring2020YanchiApplication.class);
+	}
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs5200Spring2020YanchiApplication.class, args);
+	}
+
+}
