40d455acc64559432e21186af6cc003109f496e3
zdongyu <zhou.don@husky.neu.edu>
2020-04-20 13:06:00 -0400

    123

---
+++ src/main/java/dao/MovieDao.java
@@ -0,0 +1,14 @@
+package dao;
+
+import models.*;
+
+public interface MovieDao {
+    public void truncateDatabase();
+    public User createUser(User user);
+    public Manager creatManager(Manager manager);
+    public Movie createMovie(Movie movie);
+    public Crew createMovie(Crew crew);
+    public Review createReview(Review review);
+    public User addReviewtoUser(Review review, User user);
+
+}
